713d3c315525662b50f1b618ce5f2983f2eabb78
slavny11 <slavny45@gmail.com>
2023-11-27 18:04:34 +0100

    First commit

--- LocationManager.swift
+++ LocationManager.swift
@@ -5,4 +5,40 @@
 //  Created by Viacheslav on 21/11/23.
 //
 
-import Foundation
+import SwiftUI
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    @Published var heading: CLLocationDirection = 0.0
+
+    private var locationManager = CLLocationManager()
+
+    override init() {
+        super.init()
+        setupLocationManager()
+    }
+
+    private func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+
+        if CLLocationManager.headingAvailable() {
+            locationManager.headingFilter = 1
+            locationManager.startUpdatingHeading()
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        heading = newHeading.magneticHeading
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location manager failed with error: \(error.localizedDescription)")
+    }
+}
+
+//struct CompassView: View {
+//
+//}
+
+
